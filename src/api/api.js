303573fbb085d6a270765a247e81c2f503115242
prostostasyan <67874612+prostostasyan@users.noreply.github.com>
2021-06-15 23:35:00 +0300

    Add files via upload
    optimized render

---
+++ src/api/api.js
@@ -0,0 +1,38 @@
+import axios from "axios";
+const port=3000;
+const http = `http://localhost:${port}/`
+export const getData=(category)=>{
+    return axios
+        .get(http+category)
+        .then(res=>{
+            return {data:res, error:null}
+        })
+        .catch(err=>{
+            return {data:null, error:err}
+        })
+}
+
+export const postData=(category,data)=>{
+    return axios
+        .post(http+category,{'text':data})
+        .then(res=>{
+            return {data:res, error:null}
+        })
+        .catch(err=>{
+            console.log(err)
+            return {data:null, error:err}
+        })
+}
+export const deleteData=(category,id)=>{
+    console.log(http+category+id)
+    return axios
+        .delete(http+category+id)
+        .then(res=>{
+
+            return {data:res, error:null}
+        })
+        .catch(err=>{
+            console.log(err)
+            return {data:null, error:err}
+        })
+}
